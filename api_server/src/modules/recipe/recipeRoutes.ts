1cd457aee3099b676f70b961236e7a741b39d0e5
Elor-Itz <elor2200@gmail.com>
2025-03-25 18:48:16 +0200

    Refractor

--- api_server/src/modules/recipe/recipeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateRecipe } from './recipeController';
-
-const router = express.Router();
-
-router.post('/generate', generateRecipe);
-
-export default router;
