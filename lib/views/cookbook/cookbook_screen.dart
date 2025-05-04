525c9fb0483c337470aac228baa81436bacaec75
Elor-Itz <elor2200@gmail.com>
2025-05-04 23:20:31 +0300

    Redesigned recipe cards

--- lib/views/cookbook/cookbook_screen.dart
+++ lib/views/cookbook/cookbook_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../../viewmodels/auth_viewmodel.dart';
 import '../../viewmodels/cookbook_viewmodel.dart';
 import 'view_recipe_screen.dart';
+import '../../theme/colors.dart';
 
 class CookbookScreen extends StatefulWidget {
   const CookbookScreen({super.key});
@@ -17,7 +18,8 @@ class _CookbookScreenState extends State<CookbookScreen> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final authViewModel = Provider.of<AuthViewModel>(context, listen: false);
-      final cookbookViewModel = Provider.of<CookbookViewModel>(context, listen: false);
+      final cookbookViewModel =
+          Provider.of<CookbookViewModel>(context, listen: false);
 
       // Ensure the cookbookId is not null
       final cookbookId = authViewModel.cookbookId;
@@ -31,11 +33,13 @@ class _CookbookScreenState extends State<CookbookScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cookbook', style: TextStyle(fontWeight: FontWeight.bold)),
+        title: const Text('Cookbook',
+            style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         iconTheme: const IconThemeData(color: Colors.black),
       ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Consumer<CookbookViewModel>(
         builder: (context, cookbookViewModel, child) {
           if (cookbookViewModel.isLoading) {
@@ -55,22 +59,104 @@ class _CookbookScreenState extends State<CookbookScreen> {
             itemCount: cookbookViewModel.recipes.length,
             itemBuilder: (context, index) {
               final recipe = cookbookViewModel.recipes[index];
+              final rating = recipe.rating?.toDouble() ?? 0.0;
               return Card(
-                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                color: primarySwatch[900],
                 child: ListTile(
-                  leading: recipe.imageURL != null && recipe.imageURL!.isNotEmpty
-                      ? Image.network(
-                          recipe.imageURL!,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return const Icon(Icons.image_not_supported);
+                  leading:
+                      recipe.imageURL != null && recipe.imageURL!.isNotEmpty
+                          ? Image.network(
+                              recipe.imageURL!,
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                              errorBuilder: (context, error, stackTrace) {
+                                return const Icon(Icons.image_not_supported);
+                              },
+                            )
+                          : const Icon(Icons.image),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(recipe.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: List.generate(
+                          5,
+                          (index) {
+                            if (index < rating.floor()) {
+                              // Full star
+                              return const Icon(Icons.star,
+                                  size: 20, color: primaryColor);
+                            } else if (index < rating &&
+                                rating - index >= 0.5) {
+                              // Half star
+                              return const Icon(Icons.star_half,
+                                  size: 20, color: primaryColor);
+                            } else {
+                              // Empty star
+                              return const Icon(Icons.star_border,
+                                  size: 20, color: Colors.grey);
+                            }
                           },
-                        )
-                      : const Icon(Icons.image),
-                  title: Text(recipe.title, style: const TextStyle(fontWeight: FontWeight.bold)),
-                  subtitle: Text(recipe.description),
+                        ),
+                      ),
+                    ],
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.restaurant,
+                              size: 16, color: Colors.grey),
+                          const SizedBox(width: 4),
+                          Text(
+                            recipe.mealType,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(width: 8),
+                          const Icon(Icons.room_service,
+                              size: 16, color: Colors.grey),
+                          const SizedBox(width: 4),
+                          Text(
+                            recipe.cuisineType,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(width: 8),
+                          const Icon(Icons.emoji_events,
+                              size: 16, color: Colors.grey),
+                          const SizedBox(width: 4),
+                          Text(
+                            recipe.difficulty,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          const Icon(Icons.timer, size: 16, color: Colors.grey),
+                          const SizedBox(width: 4),
+                          Text(
+                            '${recipe.cookingTime}m',
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(width: 8),
+                          const Icon(Icons.access_time,
+                              size: 16, color: Colors.grey),
+                          const SizedBox(width: 4),
+                          Text(
+                            '${recipe.prepTime}m',
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     Navigator.push(
                       context,
