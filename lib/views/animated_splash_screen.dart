dffe79d72aac81135c57578d60ab4e11f8c72679
Elor-Itz <elor2200@gmail.com>
2025-05-17 14:00:53 +0300

    Screen now takes full space

--- lib/views/animated_splash_screen.dart
+++ lib/views/animated_splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AnimatedSplashScreen extends StatefulWidget {
   final bool isLoggedIn;
@@ -16,12 +17,16 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
   @override
   void initState() {
     super.initState();
+
+    // Enable immersive mode for full screen (behind system bars)
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
     _controller = AnimationController(
       duration: const Duration(seconds: 2),
       vsync: this,
     );
 
-    // Squish: normal -> squished -> normal
+    // Vertical squish: normal -> squished -> normal
     _squishAnimation = TweenSequence([
       TweenSequenceItem(
         tween: Tween<double>(begin: 1.0, end: 0.7).chain(CurveTween(curve: Curves.easeIn)),
@@ -36,6 +41,8 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
     _controller.forward();
 
     Future.delayed(const Duration(seconds: 2), () {
+      // Restore system UI before navigating away
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
       Navigator.of(context).pushReplacementNamed(
         widget.isLoggedIn ? '/main' : '/login',
       );
@@ -45,6 +52,8 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
   @override
   void dispose() {
     _controller.dispose();
+    // Restore system UI just in case
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     super.dispose();
   }
 
@@ -52,23 +61,33 @@ class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
   Widget build(BuildContext context) {    
     return Scaffold(
       backgroundColor: const Color(0xFFF47851),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform.scale(
-              scaleY: 1.0,
-              scaleX: _squishAnimation.value,
-              child: child,
-            );
-          },
-          child: Image.asset(
-            'assets/images/splash_icon.png',
-            width: 300,
-            height: 300,
-            fit: BoxFit.contain,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: const Color(0xFFF47851),
+            ),
           ),
-        ),
+          Center(
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.scale(
+                  scaleY: _squishAnimation.value,
+                  scaleX: 1.0,
+                  child: child,
+                );
+              },
+              child: Image.asset(
+                'assets/images/splash_icon.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
