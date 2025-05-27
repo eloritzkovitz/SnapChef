cbdb00f406e6da605c58b6434406772ee33ec9e4
Elor-Itz <elor2200@gmail.com>
2025-05-27 18:56:43 +0300

    Added sharer details

--- lib/views/cookbook/view_shared_recipe_screen.dart
+++ lib/views/cookbook/view_shared_recipe_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/recipe.dart';
 import '../../models/shared_recipe.dart';
+import '../../services/user_service.dart';
 import '../../utils/image_util.dart';
 import '../../viewmodels/cookbook_viewmodel.dart';
 import '../../viewmodels/user_viewmodel.dart';
@@ -21,54 +22,30 @@ class ViewSharedRecipeScreen extends StatefulWidget {
 
 class _ViewSharedRecipeScreenState extends State<ViewSharedRecipeScreen> {
   late String? _currentImageUrl;
+  String? _sharedByName;
+  String? _sharedByProfilePic;
 
   @override
   void initState() {
     super.initState();
     _currentImageUrl = widget.sharedRecipe.recipe.imageURL;
+    _fetchSharerName();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final recipe = widget.sharedRecipe.recipe;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Shared Recipe',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        iconTheme: const IconThemeData(color: Colors.black),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.bookmark_add),
-            tooltip: 'Save Recipe to Cookbook',
-            onPressed: () => _saveRecipeToCookbook(context),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: DisplayRecipeWidget(
-                recipeObject: recipe,
-                imageUrl: ImageUtil().getFullImageUrl(_currentImageUrl),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Text(
-                'Shared by: ${widget.sharedRecipe.fromUser}',
-                style: const TextStyle(fontSize: 14, color: Colors.grey),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  Future<void> _fetchSharerName() async {
+    final userService = UserService();
+    try {
+      final user = await userService.getUserProfile(widget.sharedRecipe.fromUser);
+      setState(() {
+        _sharedByName = '${user.firstName} ${user.lastName}'.trim();
+        _sharedByProfilePic = user.profilePicture;
+      });
+    } catch (e) {
+      setState(() {
+        _sharedByName = null;
+        _sharedByProfilePic = null;
+      });
+    }
   }
 
   // Save the shared recipe to the user's cookbook
@@ -101,6 +78,78 @@ class _ViewSharedRecipeScreenState extends State<ViewSharedRecipeScreen> {
 
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(content: Text('Recipe saved to cookbook!')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final recipe = widget.sharedRecipe.recipe;
+    final sharedBy = _sharedByName ?? widget.sharedRecipe.fromUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Shared Recipe',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.bookmark_add),
+            tooltip: 'Save Recipe to Cookbook',
+            onPressed: () => _saveRecipeToCookbook(context),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: DisplayRecipeWidget(
+                recipeObject: recipe,
+                imageUrl: ImageUtil().getFullImageUrl(_currentImageUrl),
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Shadow separator styled like bottom navigation
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 2,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.grey[200],
+                      backgroundImage: (_sharedByProfilePic != null && _sharedByProfilePic!.isNotEmpty)
+                          ? NetworkImage(ImageUtil().getFullImageUrl(_sharedByProfilePic!))
+                          : const AssetImage('assets/images/default_profile.png') as ImageProvider,
+                    ),
+                    const SizedBox(width: 12),
+                    Text(
+                      'Shared by: $sharedBy',
+                      style: const TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
