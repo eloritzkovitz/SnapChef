46a5f0b741668d958c46e08f668fdaee5e0b3922
Elor-Itz <elor2200@gmail.com>
2025-05-29 18:56:14 +0300

    Updated notifications to be dismissable

--- lib/views/notifications/notifications_screen.dart
+++ lib/views/notifications/notifications_screen.dart
@@ -1,14 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:snapchef/views/notifications/upcoming_alerts_screen.dart';
-import 'friend_requests_screen.dart';
-import '../../widgets/snapchef_appbar.dart';
-import '../../models/notifications/friend_notification.dart';
-import '../../models/notifications/share_notification.dart';
-import '../../models/user.dart';
-import '../../utils/image_util.dart';
+import './friend_requests_screen.dart';
+import '../../models/notifications/app_notification.dart';
 import '../../viewmodels/notifications_viewmodel.dart';
-import '../../viewmodels/user_viewmodel.dart';
+import '../../widgets/snapchef_appbar.dart';
+import 'widgets/notification_list_item.dart';
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
@@ -18,14 +16,23 @@ class NotificationsScreen extends StatefulWidget {
 }
 
 class _NotificationsScreenState extends State<NotificationsScreen> {
+  AppNotification? _recentlyDeleted;
+  Timer? _undoTimer;
+
+  @override
+  void dispose() {
+    _undoTimer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final userViewModel = Provider.of<UserViewModel>(context, listen: false);
+    // No need to get userViewModel here, NotificationListItem will use Provider
 
     return Scaffold(
       appBar: SnapChefAppBar(
         title: const Text('Notifications',
-            style: TextStyle(fontWeight: FontWeight.bold)),          
+            style: TextStyle(fontWeight: FontWeight.bold)),
         actions: [
           IconButton(
             icon: const Icon(Icons.people),
@@ -77,139 +84,40 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
               separatorBuilder: (_, __) => const SizedBox(height: 12),
               itemBuilder: (context, index) {
                 final notif = notifications[index];
-                IconData icon;
-                Color color;
-                Widget leadingWidget;
+                return NotificationListItem(
+                  notification: notif,
+                  confirmDismiss: (direction) async {
+                    _recentlyDeleted = notif;
+                    return true;
+                  },
+                  onDismissed: (direction) async {
+                    final viewModel = Provider.of<NotificationsViewModel>(context, listen: false);
+                    await viewModel.deleteNotification(notif.id);
 
-                if (notif is ShareNotification) {
-                  icon = Icons.restaurant_menu;
-                  color = Colors.orange;
-                  // Use senderId to get the sender's profile
-                  User? sender;
-                  try {
-                    sender = userViewModel.friends
-                        .firstWhere((u) => u.id == notif.senderId);
-                  } catch (_) {
-                    sender = null;
-                  }
-                  if (sender != null &&
-                      sender.profilePicture != null &&
-                      sender.profilePicture!.isNotEmpty) {
-                    leadingWidget = CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.grey[200],
-                      backgroundImage: NetworkImage(
-                        ImageUtil().getFullImageUrl(sender.profilePicture!),
-                      ),
-                    );
-                  } else {
-                    leadingWidget = CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.grey[200],
-                      child: ClipOval(
-                        child: Image.asset(
-                          'assets/images/default_profile.png',
-                          fit: BoxFit.cover,
-                          width: 36,
-                          height: 36,
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text('Notification removed'),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () async {
+                              if (_recentlyDeleted != null) {
+                                await viewModel.addNotification(_recentlyDeleted!);
+                                _recentlyDeleted = null;
+                              }
+                            },
+                          ),
+                          duration: const Duration(seconds: 4),
                         ),
-                      ),
-                    );
-                  }
-                } else if (notif is FriendNotification) {
-                  icon = Icons.person_add_alt_1;
-                  color = Colors.green;
-                  User? friend;
-                  try {
-                    friend = userViewModel.friends
-                        .firstWhere((u) => u.fullName == notif.friendName);
-                  } catch (_) {
-                    friend = null;
-                  }
-                  if (friend != null &&
-                      friend.profilePicture != null &&
-                      friend.profilePicture!.isNotEmpty) {
-                    leadingWidget = CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.grey[200],
-                      backgroundImage: NetworkImage(
-                        ImageUtil().getFullImageUrl(friend.profilePicture!),
-                      ),
-                    );
-                  } else {
-                    leadingWidget = CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.grey[200],
-                      child: ClipOval(
-                        child: Image.asset(
-                          'assets/images/default_profile.png',
-                          fit: BoxFit.cover,
-                          width: 36,
-                          height: 36,
-                        ),
-                      ),
-                    );
-                  }
-                } else {
-                  icon = Icons.notifications;
-                  color = Colors.blueGrey;
-                  leadingWidget = CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.grey[200],
-                    child: Icon(icon, color: color, size: 20),
-                  );
-                }
+                      );
+                    }
 
-                return Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  elevation: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16, horizontal: 16),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        leadingWidget,
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                notif.title,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              if (notif.body.isNotEmpty)
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 4.0),
-                                  child: Text(
-                                    notif.body,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.black87,
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          '${notif.scheduledTime.toLocal().hour.toString().padLeft(2, '0')}:${notif.scheduledTime.toLocal().minute.toString().padLeft(2, '0')}',
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                    _undoTimer?.cancel();
+                    _undoTimer = Timer(const Duration(seconds: 5), () {
+                      _recentlyDeleted = null;
+                    });
+                  },
                 );
               },
             );
---
+++ lib/views/notifications/widgets/notification_list_item.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snapchef/models/notifications/app_notification.dart';
+import 'package:snapchef/models/notifications/friend_notification.dart';
+import 'package:snapchef/models/notifications/share_notification.dart';
+import 'package:snapchef/models/user.dart';
+import 'package:snapchef/utils/image_util.dart';
+import 'package:snapchef/viewmodels/user_viewmodel.dart';
+
+class NotificationListItem extends StatelessWidget {
+  final AppNotification notification;
+  final VoidCallback? onDelete;
+  final DismissDirectionCallback? onDismissed;
+  final Future<bool?> Function(DismissDirection)? confirmDismiss;
+
+  const NotificationListItem({
+    super.key,
+    required this.notification,
+    this.onDelete,
+    this.onDismissed,
+    this.confirmDismiss,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final userViewModel = Provider.of<UserViewModel>(context, listen: false);
+
+    IconData icon;
+    Color color;
+    Widget leadingWidget;
+
+    if (notification is ShareNotification) {
+      icon = Icons.restaurant_menu;
+      color = Colors.orange;
+      User? sender;
+      try {
+        sender = userViewModel.friends
+            .firstWhere((u) => u.id == (notification as ShareNotification).senderId);
+      } catch (_) {
+        sender = null;
+      }
+      if (sender != null &&
+          sender.profilePicture != null &&
+          sender.profilePicture!.isNotEmpty) {
+        leadingWidget = CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.grey[200],
+          backgroundImage: NetworkImage(
+            ImageUtil().getFullImageUrl(sender.profilePicture!),
+          ),
+        );
+      } else {
+        leadingWidget = CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.grey[200],
+          child: ClipOval(
+            child: Image.asset(
+              'assets/images/default_profile.png',
+              fit: BoxFit.cover,
+              width: 36,
+              height: 36,
+            ),
+          ),
+        );
+      }
+    } else if (notification is FriendNotification) {
+      icon = Icons.person_add_alt_1;
+      color = Colors.green;
+      User? friend;
+      try {
+        friend = userViewModel.friends
+            .firstWhere((u) => u.fullName == (notification as FriendNotification).friendName);
+      } catch (_) {
+        friend = null;
+      }
+      if (friend != null &&
+          friend.profilePicture != null &&
+          friend.profilePicture!.isNotEmpty) {
+        leadingWidget = CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.grey[200],
+          backgroundImage: NetworkImage(
+            ImageUtil().getFullImageUrl(friend.profilePicture!),
+          ),
+        );
+      } else {
+        leadingWidget = CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.grey[200],
+          child: ClipOval(
+            child: Image.asset(
+              'assets/images/default_profile.png',
+              fit: BoxFit.cover,
+              width: 36,
+              height: 36,
+            ),
+          ),
+        );
+      }
+    } else {
+      icon = Icons.notifications;
+      color = Colors.blueGrey;
+      leadingWidget = CircleAvatar(
+        radius: 20,
+        backgroundColor: Colors.grey[200],
+        child: Icon(icon, color: color, size: 20),
+      );
+    }
+
+    return Dismissible(
+      key: ValueKey(notification.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        color: Colors.red,
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      confirmDismiss: confirmDismiss,
+      onDismissed: onDismissed,
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              leadingWidget,
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      notification.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    if (notification.body.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: Text(
+                          notification.body,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                '${notification.scheduledTime.toLocal().hour.toString().padLeft(2, '0')}:${notification.scheduledTime.toLocal().minute.toString().padLeft(2, '0')}',
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
