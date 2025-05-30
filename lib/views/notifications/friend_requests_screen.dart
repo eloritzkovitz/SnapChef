e61ca3001949c9a37602fe4cecb4600fc0ea139a
Elor-Itz <elor2200@gmail.com>
2025-05-30 23:57:07 +0300

    Update

--- lib/views/notifications/friend_requests_screen.dart
+++ lib/views/notifications/friend_requests_screen.dart
@@ -331,20 +331,6 @@ class _FriendRequestsScreenState extends State<FriendRequestsScreen> {
                         await Provider.of<FriendViewModel>(context,
                                 listen: false)
                             .respondToRequest(req.id, true, currentUser.id);
-                        // Add notification for new friendship (to current user)
-                        await notificationsViewModel.addNotification(
-                          FriendNotification(
-                            id: await notificationsViewModel
-                                .generateUniqueNotificationId(),
-                            title: 'You are now friends with ${user.fullName}',
-                            body:
-                                'You and ${user.fullName} can now share recipes!',
-                            scheduledTime: DateTime.now(),
-                            friendName: user.fullName,
-                            senderId: currentUser.id,
-                            recipientId: user.id,
-                          ),
-                        );
                         // Add notification for the other user (the one who sent the request)
                         await notificationsViewModel.addNotification(
                           FriendNotification(
@@ -356,8 +342,8 @@ class _FriendRequestsScreenState extends State<FriendRequestsScreen> {
                                 'You and ${currentUser.fullName} can now share recipes!',
                             scheduledTime: DateTime.now(),
                             friendName: currentUser.fullName,
-                            senderId: user.id,
-                            recipientId: currentUser.id,
+                            senderId: currentUser.id,
+                            recipientId: user.id,
                           ),
                         );
                         if (context.mounted) {
