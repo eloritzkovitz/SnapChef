3c335401d01e9743207a3c4da50f67f2d0b283b9
Elor-Itz <elor2200@gmail.com>
2025-06-06 19:05:29 +0300

    Small fix

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'widgets/favorites_gallery.dart';
 import 'widgets/quick_actions.dart';
+import '../../providers/connectivity_provider.dart';
 import '../../theme/colors.dart';
-import '../../viewmodels/user_viewmodel.dart';
 import '../../utils/ui_util.dart';
+import '../../viewmodels/user_viewmodel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,35 +14,37 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final userViewModel = Provider.of<UserViewModel>(context);
     final userName = userViewModel.user?.firstName ?? 'User';
+    final isOffline = context.watch<ConnectivityProvider>().isOffline;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [       
-              const SizedBox(height: 24),      
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/icon_appbar.png',
-                    height: 36,
-                    width: 36,
+    backgroundColor: Colors.white,
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (isOffline) const SizedBox(height: 40),
+            const SizedBox(height: 24),
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/icon_appbar.png',
+                  height: 36,
+                  width: 36,
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'SnapChef',
+                  style: TextStyle(
+                    fontFamily: 'BerlinSansFBDemi',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: primaryColor,
                   ),
-                  const SizedBox(width: 8),
-                  const Text(
-                    'SnapChef',
-                    style: TextStyle(
-                      fontFamily: 'BerlinSansFBDemi',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32,
-                      color: primaryColor,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
+            ),
               const SizedBox(height: 8),
               Text(
                 '${UIUtil().getGreeting()}, $userName!',
