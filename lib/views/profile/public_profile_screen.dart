92f66f2458f17bc765d9dc9714bf18a7c365273a
Elor-Itz <elor2200@gmail.com>
2025-06-06 12:46:49 +0300

    Fixed viewing friend profile details

--- lib/views/profile/public_profile_screen.dart
+++ lib/views/profile/public_profile_screen.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
-import '../../models/user.dart';
+import 'package:provider/provider.dart';
 import 'widgets/profile_details.dart';
+import '../../models/user.dart' as model;
+import '../../viewmodels/user_viewmodel.dart';
+import '../../database/app_database.dart';
+import '../../providers/connectivity_provider.dart';
 
 class PublicProfileScreen extends StatelessWidget {
-  final User user;
+  final model.User user;
   const PublicProfileScreen({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'View Profile',
-          style: TextStyle(fontWeight: FontWeight.bold),
+    final database = Provider.of<AppDatabase>(context, listen: false);
+    final connectivityProvider = Provider.of<ConnectivityProvider>(context, listen: false);
+
+    return ChangeNotifierProvider(
+      create: (_) => UserViewModel(
+        database: database,
+        connectivityProvider: connectivityProvider,
+      )..fetchUserStats(userId: user.id),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'View Profile',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          iconTheme: const IconThemeData(color: Colors.black),
+          elevation: 1,
         ),
         backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        iconTheme: const IconThemeData(color: Colors.black),
-        elevation: 1,
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(16.0),
-          child: ProfileDetails(
-            user: user,
-            showSettings: false,
-            friendsClickable: false,
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16.0),
+            child: ProfileDetails(
+              user: user,
+              showSettings: false,
+              friendsClickable: false,
+            ),
           ),
         ),
       ),
