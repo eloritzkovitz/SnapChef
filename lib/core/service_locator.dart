39667193554317496246a3789dbe896c8ec2a24a
Elor-Itz <elor2200@gmail.com>
2025-06-21 20:05:33 +0300

    Registered viewmodels

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -20,6 +20,15 @@ import '../services/shared_recipe_service.dart';
 import '../services/sync_service.dart';
 import '../services/user_service.dart';
 import '../viewmodels/auth_viewmodel.dart';
+import '../viewmodels/cookbook_viewmodel.dart';
+import '../viewmodels/fridge_viewmodel.dart';
+import '../viewmodels/friend_viewmodel.dart';
+import '../viewmodels/ingredient_viewmodel.dart';
+import '../viewmodels/main_viewmodel.dart';
+import '../viewmodels/notifications_viewmodel.dart';
+import '../viewmodels/recipe_viewmodel.dart';
+import '../viewmodels/shared_recipe_viewmodel.dart';
+import '../viewmodels/user_viewmodel.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -29,30 +38,41 @@ void setupLocator(AppDatabase db) {
   getIt.registerSingleton<ConnectivityProvider>(ConnectivityProvider());
   getIt.registerSingleton<SyncProvider>(SyncProvider());
   getIt.registerSingleton<SyncManager>(SyncManager(getIt<ConnectivityProvider>()));
-  
-  // Ingredient Service
-  getIt.registerLazySingleton<IngredientService>(() => IngredientService());
+
+  // Main
+  getIt.registerLazySingleton<MainViewModel>(() => MainViewModel());
   
   // User  
   getIt.registerLazySingleton<AuthViewModel>(() => AuthViewModel());
+  getIt.registerLazySingleton<UserViewModel>(() => UserViewModel());
   getIt.registerLazySingleton<UserService>(() => UserService());
   getIt.registerLazySingleton<UserRepository>(() => UserRepository());
+  getIt.registerLazySingleton<FriendViewModel>(() => FriendViewModel());
   getIt.registerLazySingleton<FriendService>(() => FriendService());
+
+  // Ingredient
+  getIt.registerLazySingleton<IngredientViewModel>(() => IngredientViewModel());
+  getIt.registerLazySingleton<IngredientService>(() => IngredientService());
   
   // Fridge/Grocery
+  getIt.registerLazySingleton<FridgeViewModel>(() => FridgeViewModel());
   getIt.registerLazySingleton<FridgeService>(() => FridgeService());
   getIt.registerLazySingleton<FridgeSyncActions>(() => FridgeSyncActions(getIt<FridgeService>()));
   getIt.registerLazySingleton<GrocerySyncActions>(() => GrocerySyncActions(getIt<FridgeService>()));
   getIt.registerLazySingleton<FridgeRepository>(() => FridgeRepository());
-  
-  // Cookbook/Shared Recipes
+    
+  // Cookbook/Shared Recipes    
+  getIt.registerLazySingleton<CookbookViewModel>(() => CookbookViewModel());
   getIt.registerLazySingleton<CookbookService>(() => CookbookService());  
   getIt.registerLazySingleton<CookbookRepository>(() => CookbookRepository()); 
   getIt.registerLazySingleton<CookbookSyncActions>(() => CookbookSyncActions(getIt<CookbookRepository>()));
+  getIt.registerLazySingleton<RecipeViewModel>(() => RecipeViewModel());
+  getIt.registerLazySingleton<SharedRecipeViewModel>(() => SharedRecipeViewModel());
   getIt.registerLazySingleton<SharedRecipeService>(() => SharedRecipeService());
   getIt.registerLazySingleton<SharedRecipeRepository>(() => SharedRecipeRepository());
   getIt.registerLazySingleton<SharedRecipeSyncActions>(() => SharedRecipeSyncActions(getIt<SharedRecipeRepository>()));
 
   // Notifications
+  getIt.registerLazySingleton<NotificationsViewModel>(() => NotificationsViewModel());
   getIt.registerLazySingleton<NotificationSyncActions>(() => NotificationSyncActions(getIt<BackendNotificationService>()));
 }
